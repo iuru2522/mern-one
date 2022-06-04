cea6377bf3c10f96ce913e9e7f5010e7333a28ae
Iurii Manastyrskyi <ternoboy@gmail.com>
2022-06-04 13:23:15 -0400

    formatted with Prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from './components/navbar.component';
-import ExercisesList from './components/exercises-list.component';
-import EditExercise from './components/edit-exercise.component';
-import CreateExercise from './components/create-exercise.component';
-import CreateUser from './components/create-user.component';
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
